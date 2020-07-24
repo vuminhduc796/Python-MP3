6e9bf8c89f75f62d4abecac3db7dc16b76637ad9
Duc <mvuu0003@student.monash.edu>
2020-07-24 19:40:49 +1000

    Clean up version

--- MP3.py
+++ MP3.py
@@ -9,25 +9,36 @@ class main(object):
         self.master = master
         pygame.init()
         self.file = "test1.mp3"
-        self.play_button = tk.Button(player, width=5, height=3, text="Play", command=self.play)
-        self.play_button.grid(row = 0,column = 0)
-        self.stop_button = tk.Button(player, width=5, height=3, text="Play", command=self.exit)
-        self.stop_button.grid(row = 0, column = 1)
+        self.playing = False
         self.icon_back = PhotoImage(file="back.png").subsample(15,15)
         self.icon_next = PhotoImage(file="next.png").subsample(15,15)
-        self.stop_button = tk.Button(player, width=50, height=50, image=self.icon_back, command=self.exit)
-        self.stop_button.grid(row = 1, column = 0)
-        self.stop_button = tk.Button(player, width=50, height=50, image = self.icon_next, command=self.exit)
-        self.stop_button.grid(row = 1, column = 1)
-
+        self.icon_pause = PhotoImage(file="pause.png").subsample(15,15)
+        self.icon_resume = PhotoImage(file = "resume.png").subsample(15,15)
+        self.back_button = tk.Button(player, width=50, height=50, image=self.icon_back, command=self.resume)
+        self.back_button.grid(row = 1, column = 0,sticky = S)
+        self.resume_button = tk.Button(player, width=50, height=50, image = self.icon_resume, command=self.resume)
+        self.resume_button.grid(row = 1, column = 1)
+        self.next_button = tk.Button(player, width=50, height=50, image = self.icon_next, command=self.resume)
+        self.next_button.grid(row = 1, column = 2)
+    def resume(self):
+        if self.playing is True:
+            pygame.mixer_music.unpause()
+            self.resume_button["image"] = self.icon_pause
+            self.resume_button["command"] = self.pause
+        else:
+            self.play()
     def play(self):
+        self.playing = True
         mp3 = mutagen.mp3.MP3(self.file)
-        pygame.mixer.init(frequency=44100)
+        pygame.mixer.init(frequency=46000)
         pygame.mixer_music.load(self.file)
         pygame.mixer_music.play()
-
-    def exit(self):
-        pygame.mixer_music.stop()
+        self.resume_button["image"] = self.icon_pause
+        self.resume_button["command"] = self.pause
+    def pause(self):
+        self.resume_button["image"] = self.icon_resume
+        self.resume_button["command"] = self.resume
+        pygame.mixer_music.pause()
 
 
 
